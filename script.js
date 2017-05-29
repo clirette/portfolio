b22e28840511643f84fbad7342b16f8b7100045e
Chase Lirette <chaselirette@yahoo.com>
2017-05-28 23:52:55 -0500

    push all progress thus far

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$(".about-button").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".about").offset().top},
+	        'slow');
+	});
+
+	$(".work-button").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".work").offset().top},
+	        'slow');
+	});
+
+	$(".contact-button").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".contact").offset().top},
+	        'slow');
+	});
+
+	$('ul li').click( function() {
+    	$(this).addClass('active').siblings().removeClass('active');
+  	});
+});
