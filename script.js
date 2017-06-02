7ae0547155caf587bd42b9b27802375f4eee1712
Chase Lirette <chaselirette@yahoo.com>
2017-06-01 19:44:29 -0500

    fixed thumbnail by just adding border. Added fade to black over work-img's and pop up description text.

--- script.js
+++ script.js
@@ -20,4 +20,10 @@ $(document).ready(function() {
 	$('ul li').click( function() {
     	$(this).addClass('active').siblings().removeClass('active');
   	});
+
+  	$('.darken').hover(function() {
+    	$(this).find('img').fadeTo(500, 0.3);
+	}, function() {
+    	$(this).find('img').fadeTo(500, 1);
+	});
 });
