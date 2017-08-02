0e02c7e059e1716b59c76c80c545bdef17d6877d
Chase Lirette <chaselirette@yahoo.com>
2017-08-02 01:15:28 -0500

    toggled navbar on click, removed hr:after below certain resolution, changed container to container-fluid

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
+	$(".navbar-brand").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $(".jumbotron").offset().top},
+	        'slow');
+	});
+
 	$(".about-button").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $(".about").offset().top},
 	        'slow');
+	    $('.navbar-toggle').click();
 	});
 
 	$(".work-button").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $(".work").offset().top},
 	        'slow');
+	    $('.navbar-toggle').click();
 	});
 
 	$(".contact-button").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $(".contact").offset().top},
 	        'slow');
+	    $('.navbar-toggle').click();
 	});
 
 	$('ul li').click( function() {
